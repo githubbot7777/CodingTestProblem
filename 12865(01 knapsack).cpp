9b9a2c76a6c837cb4a625c36bf49709919776d0c
githubbot7777 <shawncan1573@gmail.com>
2023-02-10 22:34:57 +0900

    0210

---
+++ 12865(01 knapsack).cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+//https://www.acmicpc.net/problem/12865
+//설명https://www.youtube.com/watch?v=nLmhmB6NzcM&t=1460s
+//v[price,weight]
+//v[i,w]=max(v[i-1,w],v[i-1,w-w[i]]+p[i])
+#define V first
+#define W second
+using namespace std;
+int n,k;
+//dp[i][j] 에는 가방을 최대 j 무게를 채우려 할 때 0번째 물건부터 i번째 물건까지 중에서 사용되는 물건들의 최대 가치 합
+int dp[102][100002];
+
+int main(int argc, char** argv)
+{
+	cin >> n >> k;
+	vector<pair<int, int>> l(n + 1, { 0,0 });
+	for (int i = 1; i <= n; i++)
+	{
+		int w, v;
+		cin >> w >> v;
+		l[i]={ v,w };
+	}
+	
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= k; j++)
+		{
+			if (j - l[i].W >= 0)
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - l[i].W] + l[i].V);
+			else
+				dp[i][j] = dp[i - 1][j];
+		}
+	}
+	cout << dp[n][k];
+	return 0;
+}
---
+++ 15732(도토리 숨기기 parametric search).cpp
@@ -0,0 +1,53 @@
+
+#include<bits/stdc++.h>
+
+//문제 https://www.acmicpc.net/problem/15732
+//설명 https://dlwnsdud205.tistory.com/178
+
+
+using namespace std;
+
+
+int n, k, d, fr, to, gap;
+vector<tuple<int, int, int>> l;
+bool judge(int m) {
+	long long sum = 0;
+	for (int i = 0; i < k; i++) {
+		int f, t, g;
+		tie(f,t,g) = l[i];
+		if (f <= m) {
+			sum += ((min(t, m) - f) / g) + 1;
+		}	
+	}
+	return sum>=d;
+}
+
+
+ int main() {
+	 ios::sync_with_stdio(0);
+	 cin.tie(0);
+	 
+	 cin >> n >> k >> d;
+	 for(int i=0;i<k;i++)
+	 {
+		 cin >> fr >> to >> gap;
+		 l.push_back({fr,to,gap});
+	 }
+	 
+	 int l=1, h=n;
+	 while (l <= h)
+	 {
+		
+		 int m = (l + h+1) / 2;
+		 if (judge(m))
+			 h = m-1;
+		 else
+			 l = m + 1;
+
+	 }
+	
+	 cout << l;
+	 return 0;
+}
+
+
