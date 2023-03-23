256059fed1ecfeb2d85d0bc22042547942582361
githubbot7777 <shawncan1573@gmail.com>
2023-03-23 16:34:13 +0900

    0323 solving

---
+++ Progammers/정렬_가장 큰수.cpp
@@ -0,0 +1,26 @@
+﻿//https://programmers.co.kr/learn/courses/30/lessons/42746
+//참고https://mungto.tistory.com/22
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(string a, string b)
+{
+    return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+    vector<string> temp;
+    string answer = "";
+    for (auto it : numbers)
+        temp.push_back(to_string(it));
+
+    sort(temp.begin(), temp.end(), cmp);
+
+    if (temp[0] == "0") return "0";
+
+    for (auto it : temp)
+        answer += it;
+
+    return answer;
+}
