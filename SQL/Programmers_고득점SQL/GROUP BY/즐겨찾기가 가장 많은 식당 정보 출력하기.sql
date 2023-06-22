772c1fe84d21d5d4061cfc41c2dec5b28fc0e71b
githubbot7777 <107686972+githubbot7777@users.noreply.github.com>
2023-06-22 10:44:48 +0900

    Update 즐겨찾기가 가장 많은 식당 정보 출력하기.sql

--- SQL/Programmers_고득점SQL/GROUP BY/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ SQL/Programmers_고득점SQL/GROUP BY/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,4 +1,16 @@
 --문제:https://school.programmers.co.kr/learn/courses/30/lessons/131123
+--틀린 제출
+SELECT FOOD_TYPE, REST_ID, REST_NAME, MAX(FAVORITES)
+FROM REST_INFO
+GROUP BY FOOD_TYPE
+ORDER BY FOOD_TYPE DESC;
+
+/* 틀린 이유
+1. food_type으로 group by를 했으니, food_type과 MAX(Favorites)에 대한 값은 정확하게 나옵니다. 하지만 REST_ID와 REST_NAME의 경우엔 해당 그룹의 첫 번째 행에 있는 값이 나올 거에요
+2. 해당 쿼리는 mysql에서는 작동할 수 있지만 엄격한 group by에서는 작동하지 않아요. 
+   왜냐하면 group by는 원래 aggregate function이랑 함께 쓰여야 하고 지금 본인이 쓴 쿼리에서는 food_type, rest_id, rest_name이 어떤 row를 가져올지 모르는 상황입니다.
+*/
+
 /*
 IN으로 여러칼럼 비교
 SELECT *
